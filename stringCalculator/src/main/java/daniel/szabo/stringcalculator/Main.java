b741f5fd8faa9f5ac6444652af7bca7b4ce1f059
Szabo Daniel <szdani91@gmail.com>
2015-02-23 22:28:57 +0100

    metodus tud osszeadni ket szamot

--- stringCalculator/src/main/java/daniel/szabo/stringcalculator/Main.java
+++ stringCalculator/src/main/java/daniel/szabo/stringcalculator/Main.java
@@ -3,6 +3,10 @@ package daniel.szabo.stringcalculator;
 public class Main {
     
     public static void main(String[] args) {
-        
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add(""));
+        System.out.println(calculator.add("1"));
+        System.out.println(calculator.add("14"));
+        System.out.println(calculator.add("14,8"));
     }
 }
--- stringCalculator/src/main/java/daniel/szabo/stringcalculator/StringCalculator.java
+++ stringCalculator/src/main/java/daniel/szabo/stringcalculator/StringCalculator.java
@@ -3,8 +3,13 @@ package daniel.szabo.stringcalculator;
 public class StringCalculator {
 
     public int add(String numbers) {
-        if(numbers.isEmpty()) {
-            return 0;
-        } else return Integer.parseInt(numbers);
+        if(numbers.isEmpty()) return 0;
+        String[] splitNumbers = numbers.split(",");
+        if(splitNumbers.length == 1) {
+            return Integer.parseInt(splitNumbers[0]);
+        }
+        int firstNumber = Integer.parseInt(splitNumbers[0]);
+        int secondNumber = Integer.parseInt(splitNumbers[1]);
+        return firstNumber + secondNumber;
     }
 }
