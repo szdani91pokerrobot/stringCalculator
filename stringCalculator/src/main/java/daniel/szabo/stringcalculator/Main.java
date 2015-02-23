b6196d2e9f0098b534bb93b7c508dd966ce2200b
Szabo Daniel <szdani91@gmail.com>
2015-02-23 22:54:36 +0100

    metodus tud kezelni barmilyen elvalaszto jelet

--- stringCalculator/src/main/java/daniel/szabo/stringcalculator/Main.java
+++ stringCalculator/src/main/java/daniel/szabo/stringcalculator/Main.java
@@ -9,5 +9,9 @@ public class Main {
         System.out.println(calculator.add("14"));
         System.out.println(calculator.add("14,8"));
         System.out.println(calculator.add("14,8,2,9,0"));
+        System.out.println(calculator.add("14;6"));
+        System.out.println(calculator.add("14\n7"));
+        System.out.println(calculator.add("14|4"));
+        System.out.println(calculator.add("13++4++4"));
     }
 }
--- stringCalculator/src/main/java/daniel/szabo/stringcalculator/StringCalculator.java
+++ stringCalculator/src/main/java/daniel/szabo/stringcalculator/StringCalculator.java
@@ -1,10 +1,15 @@
 package daniel.szabo.stringcalculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    private static final String DEFAULT_SEPARATOR = ",";
 
     public int add(String numbers) {
         if(numbers.isEmpty()) return 0;
-        String[] splitNumbers = numbers.split(",");
+        String separator = getSeparator(numbers);
+        String[] splitNumbers = numbers.split(Pattern.quote(separator));
         if(splitNumbers.length == 1) {
             return Integer.parseInt(splitNumbers[0]);
         }
@@ -13,5 +18,24 @@ public class StringCalculator {
             sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    private String getSeparator(String incomingString) {
+        String separator = "";
+        Boolean isThereANewSeparator = false;
+        for (int i = 0; i < incomingString.length(); i++) {
+            if(!Character.isDigit(incomingString.charAt(i))) {
+                separator += incomingString.charAt(i);
+                isThereANewSeparator = true;
+            }
+            if(Character.isDigit(incomingString.charAt(i)) && isThereANewSeparator) {
+                break;
+            }
+        }
+        if(separator.isEmpty()) {
+            return DEFAULT_SEPARATOR;
+        } else {
+            return separator;
+        }
     }
 }
